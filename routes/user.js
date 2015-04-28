9c80d7dd462aa939708bd8db792c0a931372c085
godwinls <263209050@qq.com>
2015-04-27 17:13:14 -0700

    update user

--- routes/user.js
+++ routes/user.js
@@ -1,3 +1,4 @@
+var db = require('../routes/dbConnection.js');
 
 /*
  * GET users listing.
@@ -8,9 +9,26 @@ exports.signin = function(req, res){
  
 		
 exports.search = function(req, res){
-	res.render("search.ejs",{movies:null});
+	res.render("search.ejs");
 };
 exports.aftersearch = function(req, res){
-	  res.render("signin.ejs",{movies:null});
-	};
+	var depCity =req.param("DepCity");
+	var arriCity =req.param("ArriCity");
+	var depTimel = req.param("DepTimel");
+	var depTimeh = req.param("DepTimeh");
+	var sql = "SELECT * FROM Flight WHERE Flight_scity = '"+depCity+"' AND Flight_ecity = '"+arriCity+"' AND Flight_stime Between '"+
+	          depTimel+"' AND '"+depTimeh+"' AND (Flight_Fseats>0 OR Flight_Bseats>0 OR Flight_Eseats>0)";
+	
+	db.getConnection(function(err,connection){
+		var query = connection.query(sql,function(err,results){
+			if(err){
+				console.log("err message: "+ err.message);
+			}else{
+				console.log("info:"+results);
+				connection.release();		
+				res.render('searchresult.ejs',{show:results})
+			}
+		})
+	})
+};
 	
